82ea94ad579a53647931fb567e51d5ff89eb9d9e
scaler <>
2025-06-08 15:21:11 +0000

    docs: SUBMISSION_COMMIT

--- src/GetData.jsx
+++ src/GetData.jsx
@@ -2,22 +2,55 @@ import React, { useEffect } from "react";
 
 function GetData() {
   // using state management, manage
+    const [user,setUser]= useState(null);
+    
+
   // error, loading and success states
+    const [loading,setLoading]=useState(true);
+    const [error,setError] = useState(null);
 
   async function fetchData() {
-    // complete the code here
+    try{
+      const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+      if(!response.ok)
+      {
+        throw new Error('Network response was not ok');
+      }
+      const data= await response.json();
+      setUser(data);      
+    }
+    catch(err){
+       setError('Error fetching data');
+    }
+    finallay
+    {
+      setLoading(false);
+    }
   }
 
-  // call the fetch data function when the
+  // call the fetch data function when the  
   // page loads
+  useEffect(()=>{    
+    fetchData();
+  },[]);
+
+  if(loading)
+  {
+    return <h2>loading data</h2>
+  }
+
+  if(error)
+  {
+    return <h2>{error}</h2>
+  }
 
   // Here is the basic data boilerplate
   return (
     <>
       {/* do not edit the code below this line */}
-      <h2>Name: {data.name}</h2>
-      <h2>Email: {data.email}</h2>
-      <h2>Username: {data.username}</h2>
+      <h2>Name: {user?.name}</h2>
+      <h2>Email: {user?.email}</h2>
+      <h2>Username: {user?.username}</h2>
     </>
   );
 }
